0b3f9293ba5ec19ec01aa48dd59a488ea33b388b
obaid ullah <obaidullah.developer@gmail.com>
2026-09-07 19:13:54 +0500

    feat: add TeamSection component for showcasing leadership, engineering, and design teams

--- components/sections/TeamSection.tsx
+++ components/sections/TeamSection.tsx
@@ -146,7 +146,7 @@ export function TeamSection() {
           <TeamCategoryGroup
             title="Engineering"
             members={engineering}
-            columns="grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
+            columns="grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 max-w-3xl"
           />
 
           {/* Design & Product */}
